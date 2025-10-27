95df54e3d80d697e8fc00b1e6a9a60e8b90264af
Andree Villegas <villegasandree562@gmail.com>
2025-10-26 19:59:50 -0500

    Inicio del proyecto

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+
+#Instancia de la app
+app = Flask(__name__)
+
+#Ruta Principal
+@app.route('/')
+def home():
+    return "Hola"
+
+#Ejecución de la app
+if __name__ == '__main__':
+    app.run(debug=True) #Para ver errores en tiempo real 
