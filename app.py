0af43ed010104b5773e6192a7484fe67b2e2681b
Andree Villegas <villegasandree562@gmail.com>
2025-10-26 20:38:26 -0500

    Actualización #1 del código

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #Librerías
-from flask import Flask
+from flask import Flask, render_template
 
 #Instancia de la app
 app = Flask(__name__)
 
 #Ruta Principal
 @app.route('/')
-def home():
-    return "Hola"
+def index():
+    return render_template("index.html")
 
 #Ejecución de la app
 if __name__ == '__main__':
